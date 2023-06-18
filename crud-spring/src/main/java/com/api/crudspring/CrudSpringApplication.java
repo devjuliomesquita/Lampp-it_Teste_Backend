05276a15de9ee2c3a43f3a87e94ff1c9434d9afd
Júlio César Mesquita Camilo Martins <94556951+devjuliomesquita@users.noreply.github.com>
2023-06-18 13:54:52 -0300

    Realizaconfig do swagger inicial

--- crud-spring/src/main/java/com/api/crudspring/CrudSpringApplication.java
+++ crud-spring/src/main/java/com/api/crudspring/CrudSpringApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 
 @SpringBootApplication
-@OpenAPIDefinition(info = @Info(title = "Teste Backend Lampp-it", version = "1.0", description = "CRUD de funcionários e teste de Array de strings."))
+@OpenAPIDefinition(info = @Info(title = "Teste Backend Lampp-it", version = "1.0", description = "CRUD de funcionários e teste de Array de strings.", license = @License(name = "Apache 2.0", url = "https://www.apache.org/licesen.html"), contact = @Contact(name = "Júlio C. Mesquita", email = "juliocesarmcamilo@gmail.com", url = "https://github.com/devjuliomesquita")))
 public class CrudSpringApplication {
 
 	public static void main(String[] args) {
