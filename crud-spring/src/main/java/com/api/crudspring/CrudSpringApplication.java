44d4afcd87c796ce81b8eb6f9fc94fce1fe7cec7
Júlio César Mesquita Camilo Martins <94556951+devjuliomesquita@users.noreply.github.com>
2023-06-18 01:10:34 -0300

    Inicio da documentação via swagger
    A partir das versões 3 do spring boot houver problemas.

--- crud-spring/src/main/java/com/api/crudspring/CrudSpringApplication.java
+++ crud-spring/src/main/java/com/api/crudspring/CrudSpringApplication.java
@@ -3,7 +3,11 @@ package com.api.crudspring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Teste Backend Lampp-it", version = "1.0", description = "CRUD de funcionários e teste de Array de strings."))
 public class CrudSpringApplication {
 
 	public static void main(String[] args) {
